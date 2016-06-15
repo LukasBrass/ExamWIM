8e382f4033af0e007f585098d086fe1c48aa5a36
lukas.brasseleur <lukas.brasseleur@gmail.com>
2016-06-15 05:40:39 -0400

    distance done, mais long chargemet de la page

--- ex3/distance.php
+++ ex3/distance.php
@@ -5,19 +5,39 @@
   </head>
   <body>
     <h1>Distance</h1>
-    <table>
+    <table border=1>
     <thead>
       <tr>
         <th></th>
       <?php 
            include('parametres.php.inc');
        $pdo = new PDO("mysql:host=".$host.";dbname=".$bdd,$user,$password);
-        $res = $pdo->query("select dsitinct nom from capitale");
-        foreach($res as $resultat){
+        $capitale1 = $pdo->query("select distinct nom from capitale");
+        foreach($capitale1 as $resultat){
           echo "<th>".$resultat['nom']."</th>";
         }
         ?>
       </tr>
     </thead>
     <tbody>
-      
+        <?php 
+        $cap1 = $pdo->query("select distinct nom,id from capitale");
+        foreach($cap1 as $capitale1){
+          echo "<tr><td>".$capitale1['nom']."</td>";
+          $cap2 = $pdo->query("select id from capitale");
+          foreach($cap2 as $capitale2){
+            $stmt = $pdo->prepare("select distance from distance where id1 = :id1 and id2 = :id2");
+            $stmt ->bindParam(':id1',$capitale1['id']);
+            $stmt ->bindParam(':id2',$capitale2['id']);
+            $stmt -> execute();
+            while($res = $stmt -> fetch()){
+              echo'<td>'.$res['distance'].'</td>';
+            }
+          }
+          echo'</tr>';
+        }
+      ?>
+      </tbody>
+    </table>
+  </body>
+</html>
